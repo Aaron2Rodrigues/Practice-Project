71cfea55862edcc608f07cd9243af477166e499a
Chris <chrisaaronrodrigues@gmail.com>
2025-07-14 11:48:10 +0530

    Updated entity Config

---
+++ src/Salary_project/config/configuration.py
@@ -0,0 +1,5 @@
+artifact_root: artifacts
+
+DataIngestion:
+    input_path:Data/raw/salary.csv
+    output_path:Data/processed/salary_processed.csv
---
+++ src/Salary_project/entity/config_entity.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass
+from pathlib import Path 
+
+@dataclass
+class DataIngestionConfig:
+    """ Path to access the raw data """
+    root_dir:Path    
+    """ Path to store the processed data """   
+    output_path:Path
