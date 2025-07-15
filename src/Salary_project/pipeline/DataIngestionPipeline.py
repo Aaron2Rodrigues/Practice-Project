e6d4efb71962d5efe44eafe5ef5b485a57eac512
Chris <chrisaaronrodrigues@gmail.com>
2025-07-15 11:35:14 +0530

    s

--- src/Salary_project/pipeline/DataIngestionPipeline.py
+++ src/Salary_project/pipeline/DataIngestionPipeline.py
@@ -7,7 +7,7 @@ class DataIngestionTrainigPipeline:
         pass
 
     def initiate_data_ingestion(self):
-        config = ConfigurationManger()
+        config = ConfigurationManager()
         data_ingestion_config = config.get_data_ingestion_config()
         data_ingestion = DataIngestion(config=data_ingestion_config)
-        data_ingestion.download_file()
+        data_ingestion.load_file()
